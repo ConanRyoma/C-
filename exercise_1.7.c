42a49eed8e2954f327fd90ba07372aacf398ca52
ConanRyoma <90973835+ConanRyoma@users.noreply.github.com>
2022-08-22 18:35:51 +0800

    Add files via upload
    第一章的课后习题5 6 7

---
+++ exercise_1.7.c
@@ -0,0 +1,20 @@
+// 关于注释的练习 第一章的习题7
+
+#include<stdio.h>
+int main()
+{
+    printf("How do you do!\n");// 行注释
+
+    printf("How do you do!\n");/*块注释*/
+
+    printf("How do you do!\n");/*块注释
+                                 啊啊啊啊啊*/
+    // printf("How do you do!\n");
+
+    printf("//How do you do!\n");
+
+    /*printf("How do you do!\n");
+    return 0;*/
+
+    return 0;
+}
